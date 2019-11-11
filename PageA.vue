f9ad957fd80d689fa87875adf64e6eb69ed4977c
1Wwo3Yai1Tni4 <57606974+1Wwo3Yai1Tni4@users.noreply.github.com>
2019-11-11 10:30:58 +0800

    Add files via upload

---
+++ PageA.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>pageA{{msg}}</h1>
+        <span>{{param}}</span>
+        <span>{{query}}</span>
+        <button @click="back">返回</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PageA",
+       data(){
+            return{
+                msg:"2222",
+                param: this.$route.params,
+                query: this.$route.query
+            }
+       },
+        beforeRouteEnter(to, from, next){
+            console.log(this);
+            next(vm=>{
+                vm.msg="888";
+            })
+        },
+        methods:{
+            back:function () {
+                //this.$router.replace({path:'/pageB',query:{abc:"123"}})
+                this.$router.push({name:'pageB',params:{abc:"123"}})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
